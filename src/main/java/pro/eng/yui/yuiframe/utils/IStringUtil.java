12aee82cb6a799f1e55d6462e1da02ae7414c489
yui-Kitamura <yui@yui-kitamura.eng.pro>
2023-05-05 12:58:21 +0900

    impl StringUtils

--- src/main/java/pro/eng/yui/yuiframe/utils/IStringUtil.java
+++ src/main/java/pro/eng/yui/yuiframe/utils/IStringUtil.java
@@ -1,5 +1,7 @@
 package pro.eng.yui.yuiframe.utils;
 
+import pro.eng.yui.yuiframe.utils.impl.StringUtils;
+
 /**
  * 汎用的な文字列操作を定義する
  * @since 1.0.0
@@ -12,6 +14,43 @@ public interface IStringUtil {
     String EMPTY = "";
     /** one Space string <code>" "</code> */
     String SPACE = " ";
+
+    /** test the two strings are equal. <code>null</code>-safe
+     * @see String#equalsIgnoreCase(String)  */
+    boolean eq(final String first, final String second);
+
+    /**
+     * get joined string
+     * @param arg any list or arrays to concat
+     * @return single string value that joined all arguments
+     */
+    String concat(Iterable<? extends CharSequence> arg);
+    /** @see IStringUtil#concat(Iterable) */
+    String concat(CharSequence... arg);
+
+    /**
+     * get joined string that each separated with delimiter
+     * @param delimiter joint string
+     * @param arg any list or arrays to concat
+     * @return single string value that joined all arguments each separated with delimiter
+     */
+    String concatWithDelimiter(CharSequence delimiter, Iterable<? extends CharSequence> arg);
+    /** @see IStringUtil#concatWithDelimiter(CharSequence, Iterable) */
+    String concatWithDelimiter(CharSequence delimiter, CharSequence... arg);
+
+    /**
+     * Check the value is empty.
+     * @param value the value that you want to test
+     * @return if <code>null</code> or length is 0 then <code>true</code>, otherwise <code>false</code>
+     */
+    boolean isEmpty(String value);
+    /**
+     * Check the objects string representation is empty.
+     * @param obj the object that you want to test
+     * @return <code>true</code> when null or the {@link Object#toString()} value is empty
+     * @see StringUtils#isEmpty(String)
+     */
+    boolean isEmpty(Object obj);
 
 
 }
--- src/main/java/pro/eng/yui/yuiframe/utils/impl/StringUtils.java
+++ src/main/java/pro/eng/yui/yuiframe/utils/impl/StringUtils.java
@@ -1,6 +1,9 @@
 package pro.eng.yui.yuiframe.utils.impl;
 
+import pro.eng.yui.yuiframe.YuiFrame;
 import pro.eng.yui.yuiframe.utils.IStringUtil;
+
+import java.util.List;
 
 /**
  * 汎用的な文字列操作の実装
@@ -15,7 +18,7 @@ public class StringUtils implements IStringUtil {
     static{
         instance = new StringUtils();
     }
-    public static StringUtils getInstance(){
+    /* project */ public static StringUtils getInstance(){
         return instance;
     }
     private StringUtils(){
@@ -25,5 +28,92 @@ public class StringUtils implements IStringUtil {
     // fields
 
     // methods
+    @Override
+    public boolean eq(final String first, final String second){
+        if(first == null && second == null){
+            return true;
+        }
+        if( first == null /* && second != null */ ||
+                /* first != null && */ second == null ){ //片方がnull
+            return false;
+        }
+        return first.equalsIgnoreCase(second);
+    }
 
+
+    @Override
+    public String concat(Iterable<? extends CharSequence> arg){
+        for(CharSequence cs : arg){
+            if(cs == null){
+                cs = SPACE;
+            }
+        }
+        return join(EMPTY, arg);
+    }
+    @Override
+    public String concat(CharSequence... arg){
+        for(int idx = 0; idx < arg.length; idx++){
+            if(arg[idx] == null){
+                arg[idx] = SPACE;
+            }
+        }
+        return join(EMPTY, arg);
+    }
+
+    @Override
+    public String concatWithDelimiter(CharSequence delimiter, Iterable<? extends CharSequence> arg){
+        for(CharSequence cs : arg){
+            if(cs == null){
+                cs = SPACE;
+            }
+        }
+        return join(delimiter, arg);
+    }
+    @Override
+    public String concatWithDelimiter(CharSequence delimiter, CharSequence... arg){
+        for(int idx = 0; idx < arg.length; idx++){
+            if(arg[idx] == null){
+                arg[idx] = SPACE;
+            }
+        }
+        return join(delimiter, arg);
+    }
+
+    /**
+     * join the arguments with delimiter
+     * @param delimiter value that separates each argument. If <code>null</code> then replaced to one space.
+     * @param arg list of value that to be joined. if the value is <code>null</code> then replaced to one space.
+     * @return one string value that joins all arguments each separated by delimiter
+     */
+    private String join(final CharSequence delimiter, final Iterable<? extends CharSequence> arg){
+        String delimitStr = (delimiter == null) ? SPACE : delimiter.toString();
+        StringBuilder sb = new StringBuilder();
+        for(int idx = 0; arg.iterator().hasNext(); idx++){
+            CharSequence cs = arg.iterator().next();
+            if(cs == null){
+                sb.append(SPACE);
+            }else{
+                sb.append(cs);
+            }
+            if(arg.iterator().hasNext()){
+                sb.append(delimitStr);
+            }
+        }
+        return sb.toString();
+    }
+
+    /** @see StringUtils#join(CharSequence, Iterable) */
+    private String join(final CharSequence delimiter, final CharSequence... arg){
+        List<CharSequence> argList = YuiFrame.ListUtil.toList(arg);
+        return join(delimiter, argList);
+    }
+
+    @Override
+    public boolean isEmpty(String value){
+        return value == null || value.length() == 0;
+    }
+    @Override
+    public boolean isEmpty(Object obj){
+        return obj == null || isEmpty(obj.toString());
+    }
 }
