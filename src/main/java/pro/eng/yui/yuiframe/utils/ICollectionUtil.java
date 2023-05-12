fb89eb3b3046f8cfc608f4d3c9f7ad65136f4a8b
yui-Kitamura <yui@yui-kitamura.eng.pro>
2023-05-12 17:41:01 +0900

    add define and implement of CollectionUtils

--- src/main/java/pro/eng/yui/yuiframe/utils/ICollectionUtil.java
+++ src/main/java/pro/eng/yui/yuiframe/utils/ICollectionUtil.java
@@ -1,5 +1,7 @@
 package pro.eng.yui.yuiframe.utils;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -10,5 +12,97 @@ import java.util.List;
  */
 public interface ICollectionUtil {
 
-    <T> List<T> toList(T... aryObj);
+    /**
+     * Convert array to {@link java.util.List}
+     * @param aryObj values array
+     * @param <T>    Type of parameters
+     * @return an {@link java.util.List} that contains all arguments
+     * @see java.util.List#of(Object[])
+     */
+    <T> List<T> toList(final T... aryObj);
+
+    /**
+     * Convert array to {@link java.util.ArrayList}
+     * @param aryObj values array
+     * @param <T>    Type of parameters
+     * @return an {@link java.util.List} that contains all arguments
+     * @see java.util.List#of(Object[])
+     */
+    <T> ArrayList<T> toArrayList(final T... aryObj);
+
+    /**
+     * judge array is empty ( <code>null</code> or length is Zero )
+     * @param array target
+     * @return array is null or nothing in collection then <code>true</code>
+     */
+    boolean isEmpty(final Object[] array);
+    /**
+     * judge Collection is empty
+     * @param list target instance
+     * @return Collection is null or nothing in it then <code>true</code>
+     */
+    boolean isEmpty(final Collection<?> list);
+
+    /**
+     * Format collection instance to following format.
+     * <pre>
+     * [entity1.toString(), entity2.toString(), ...]
+     * </pre>
+     * For example, {@linkplain StringBuilder} then like
+     * <pre>
+     * [sb1sb1sb1, sb2sb2sb2]
+     * </pre>
+     * If the entity are the Collection,then call this method for recursively
+     * @param collection a collection instance that format to string
+     * @return single line string value
+     */
+    String toString(final Collection<?> collection);
+
+    /**
+     * Format collection instance to following format.
+     * <pre>
+     * {size: 2, data: [entity1.toString(), entity2.toString()]}
+     * </pre>
+     * @param collection a collection instance that format to string
+     * @return single line string value
+     */
+    String toStrMeta(final Collection<?> collection);
+
+    /**
+     * Format collection instance to following format.
+     * <pre>
+     * [
+     * entity1.toString(),
+     * entity2.toString(),
+     * ...
+     * ]
+     * </pre>
+     * For example, {@linkplain StringBuilder} then like
+     * <pre>
+     * [
+     * sb1sb1sb1,
+     * sb2sb2sb2
+     * ]
+     * </pre>
+     * If the entity are the Collection,then call this method for recursively
+     * @param collection a collection instance that format to string
+     * @return multiple line string value
+     */
+    String toStringBr(final Collection<?> collection);
+
+    /**
+     * Format collection instance to following format.
+     * <pre>
+     * {
+     * size: 2,
+     * data: [
+     * entity1.toString(),
+     * entity2.toString()
+     * ]}
+     * </pre>
+     * @param collection a collection instance that format to string
+     * @return multiple line string value
+     */
+    String toStrMetaBr(final Collection<?> collection);
+
 }
--- src/main/java/pro/eng/yui/yuiframe/utils/impl/CollectionUtils.java
+++ src/main/java/pro/eng/yui/yuiframe/utils/impl/CollectionUtils.java
@@ -1,8 +1,9 @@
 package pro.eng.yui.yuiframe.utils.impl;
 
+import pro.eng.yui.yuiframe.YuiFrame;
 import pro.eng.yui.yuiframe.utils.ICollectionUtil;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Implement of common operation for Collections
@@ -28,7 +29,82 @@ public class CollectionUtils implements ICollectionUtil {
 
     // methods
     @Override
-    public <T> List<T> toList(T... aryObj) {
-        return List.of(aryObj);
+    public <T> List<T> toList(final T... aryObj) {
+        return Arrays.asList(aryObj);
     }
+
+    @Override
+    public <T> ArrayList<T> toArrayList(final T... aryObj) {
+        return new ArrayList<>(Arrays.asList(aryObj));
+    }
+
+    @Override
+    public boolean isEmpty(final Object[] array) {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty(final Collection<?> list) {
+        if(YuiFrame.ObjectUtil.isNull(list)){
+            return true;
+        }
+        return list.isEmpty();
+    }
+
+    @Override
+    public String toString(final Collection<?> collection) {
+        return collection2Str(collection, false, false);
+    }
+
+    @Override
+    public String toStrMeta(final Collection<?> collection) {
+        return collection2Str(collection, true, false);
+    }
+
+    @Override
+    public String toStringBr(final Collection<?> collection) {
+        return collection2Str(collection, false, true);
+    }
+
+    @Override
+    public String toStrMetaBr(final Collection<?> collection) {
+        return collection2Str(collection, true, true);
+    }
+
+    private <E> String collection2Str(final Collection<E> collection, boolean meta, boolean br){
+        StringBuilder sb = new StringBuilder();
+        if(meta){
+            sb.append("{");
+            if(br){ sb.append(System.lineSeparator()); }
+            sb.append("size: ").append(YuiFrame.ObjectUtil.isNull(collection)? "0":collection.size()).append(",");
+            if(br){ sb.append(System.lineSeparator()); }
+            sb.append("data: ");
+        }
+        boolean isObjCollection;
+        try{
+            E obj = collection.stream().toList().get(collection.size()-1);
+            isObjCollection = YuiFrame.ObjectUtil.isTypeOf(obj, Collection.class) || obj.getClass().isArray();
+        } catch (IndexOutOfBoundsException e){
+            isObjCollection = false;
+        }
+        sb.append("[");
+        List<E> colStream = collection.stream().toList();
+        for(int idx=0; idx<collection.size(); idx++){
+            if (isObjCollection) {
+                sb.append(collection2Str((Collection<?>) colStream.get(idx), meta, br));
+            }else{
+                sb.append(colStream.get(idx).toString());
+            }
+            if(idx < collection.size()){
+                sb.append(",");
+            }
+            if(br){ sb.append(System.lineSeparator()); }
+        }
+        sb.append("]");
+        if(meta){
+            sb.append("}");
+        }
+        return sb.toString();
+    }
+
 }
