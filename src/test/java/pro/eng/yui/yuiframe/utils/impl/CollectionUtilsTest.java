ed6b1225dfb6b430ed820c6897814cd52fcfffa5
yui-Kitamura <yui@yui-kitamura.eng.pro>
2023-05-12 18:11:05 +0900

    テスト用Collectionを定義

--- src/test/java/pro/eng/yui/yuiframe/utils/impl/CollectionUtilsTest.java
+++ src/test/java/pro/eng/yui/yuiframe/utils/impl/CollectionUtilsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import pro.eng.yui.yuiframe.YuiFrame;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,6 +29,24 @@ class CollectionUtilsTest {
 
 
     /* ** toString(col) ** */
+    private static class MyCls{
+        private int val;
+        public MyCls(int value){
+            val = value;
+        }
+        @Override
+        public String toString() {
+            return "this is MyCls No."+ val;
+        }
+    }
+    private final List<String> nullCollection = null;
+    private final List<String> emptyCollection = new ArrayList<>(3);
+    private final List<String> collection = YuiFrame.CollectionUtil.toArrayList("str1","str2", null, "str4");
+    private final List<List<MyCls>> inCol = YuiFrame.CollectionUtil.toList(
+            YuiFrame.CollectionUtil.toList(new MyCls(1), new MyCls(2), new MyCls(3)),
+            YuiFrame.CollectionUtil.toList(new MyCls(4), new MyCls(5)),
+            YuiFrame.CollectionUtil.toList(new MyCls(6))
+    );
 
 
     /* ** toStrMeta(col) ** */
